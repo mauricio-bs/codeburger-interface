79b7553e089d96e380d7de207077ce51d40fcf80
mauricio-bs <mauricio.3g2014@gmail.com>
2021-12-16 16:35:36 -0300

    Created interceptor to insert the user token on api calls

--- src/services/api.js
+++ src/services/api.js
@@ -5,4 +5,12 @@ const apiodeBurger = axios.create({
   validateStatus: () => true
 })
 
+apiodeBurger.interceptors.request.use(async config => {
+  const userData = await localStorage.getItem('codeburger:userData')
+  const token = userData && JSON.parse(userData).token
+
+  config.headers.authorization = `Bearer ${token}`
+  return config
+})
+
 export default apiodeBurger
