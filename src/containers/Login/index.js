ba88fdafea03c5469cf8ad8785301eba1199440b
mauricio-bs <mauricio.3g2014@gmail.com>
2021-12-16 16:35:12 -0300

    Made adjusts to login page

--- src/containers/Login/index.js
+++ src/containers/Login/index.js
@@ -1,7 +1,7 @@
 import { yupResolver } from '@hookform/resolvers/yup'
 import React from 'react'
 import { useForm } from 'react-hook-form'
-import { Link } from 'react-router-dom'
+import { Link, useHistory } from 'react-router-dom'
 import { toast } from 'react-toastify'
 import * as Yup from 'yup'
 
@@ -30,6 +30,7 @@ const schema = Yup.object().shape({
 })
 
 function Login() {
+  const history = useHistory()
   const { putUserData } = useUser()
 
   const {
@@ -52,6 +53,10 @@ function Login() {
     )
 
     putUserData(data)
+
+    setTimeout(() => {
+      history.push('/')
+    }, 1000)
   }
 
   return (
